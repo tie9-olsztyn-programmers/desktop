4408d4a2022bf5d06fd42e06c1eaea556597f018
jarek <jarek@>
2025-04-22 21:53:21 +0200

    Deploy website - based on 992b6b35958c511df3715760b9b33f247e50713b

---
+++ assets/js/5e95c892.1c9b1c26.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdesktop=self.webpackChunkdesktop||[]).push([[9647],{8122:(e,s,r)=>{r.r(s),r.d(s,{default:()=>p});r(6540);var t=r(4164),u=r(1082),a=r(204),c=r(2831),d=r(1410),n=r(4848);function p(e){return(0,n.jsx)(u.e3,{className:(0,t.A)(a.G.wrapper.docsPages),children:(0,n.jsx)(d.A,{children:(0,c.v)(e.route.routes)})})}}}]);
